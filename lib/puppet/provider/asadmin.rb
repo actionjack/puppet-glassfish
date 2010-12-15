13df25633ab0bb92d003bf00c9bc1a4b45bcf88a
actionjack <martin@uncommonsense-uk.com>
2010-12-15 13:48:47 +0000

    backing out hardcoded asadmin will update spec file instead to place softlinks to glassfish/bin/* file in /usr/bin instead

--- lib/puppet/provider/asadmin.rb
+++ lib/puppet/provider/asadmin.rb
@@ -7,7 +7,7 @@ class Puppet::Provider::Asadmin < Puppet::Provider
     args << "--passwordfile" << @resource[:passwordfile]
     passed_args.each { |arg| args << arg }
     exec_args = args.join " "
-    command = "/opt/glassfish/bin/asadmin #{exec_args}"
+    command = "asadmin #{exec_args}"
     command = "su - #{@resource[:user]} -c \"#{command}\"" if @resource[:user] and
       not command.match /create-service/
     self.debug command
